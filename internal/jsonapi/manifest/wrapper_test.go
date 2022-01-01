8d1a84b2213f5ec00ae42d55fbbdab1fff8cff7b
Lio李歐 <lionello@users.noreply.github.com>
2022-01-01 10:53:50 -0800

    Update wrapper for Nix
    RELEASE_NOTES=Use Nix profile path and pick up gpg-agent from PATH
    
    Also moving the `export` statements to the top of the file for `gpg-agent`
    
    Signed-off-by: Lionello Lunesu <lio+git@lunesu.com>

--- internal/jsonapi/manifest/wrapper_test.go
+++ internal/jsonapi/manifest/wrapper_test.go
@@ -10,16 +10,17 @@ import (
 const (
 	wrapperGolden = `#!/bin/sh
 
+export PATH="$PATH:$HOME/.nix-profile/bin" # required for Nix
+export PATH="$PATH:/usr/local/bin" # required on MacOS/brew
+export PATH="$PATH:/usr/local/MacGPG2/bin" # required on MacOS/GPGTools GPGSuite
+export GPG_TTY="$(tty)"
+
 if [ -f ~/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
 	source ~/.gpg-agent-info
 	export GPG_AGENT_INFO
 else
 	eval $(gpg-agent --daemon)
 fi
-
-export PATH="$PATH:/usr/local/bin" # required on MacOS/brew
-export PATH="$PATH:/usr/local/MacGPG2/bin" # required on MacOS/GPGTools GPGSuite
-export GPG_TTY="$(tty)"
 
 gopass-jsonapi listen
 
