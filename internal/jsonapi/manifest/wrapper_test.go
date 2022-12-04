e74784c9ce9a6d9e8ea455cf645788bbd87a9b73
Dominik Schulz <mail@dschulz.eu>
2022-12-04 17:01:22 +0100

    Fix tests
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

--- internal/jsonapi/manifest/wrapper_test.go
+++ internal/jsonapi/manifest/wrapper_test.go
@@ -15,6 +15,9 @@ export PATH="$PATH:/usr/local/bin" # required on MacOS/brew
 export PATH="$PATH:/usr/local/MacGPG2/bin" # required on MacOS/GPGTools GPGSuite
 export GPG_TTY="$(tty)"
 
+# Uncomment to debug gopass-jsonapi
+# export GOPASS_DEBUG_LOG=/tmp/gopass-jsonapi.log
+
 if [ -f ~/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
 	source ~/.gpg-agent-info
 	export GPG_AGENT_INFO
