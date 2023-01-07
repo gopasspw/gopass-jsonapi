fa662dbaeb9965856b2236c5121625aa0c2bf8a6
Dominik Schulz <mail@dschulz.eu>
2023-01-07 22:14:44 +0100

    Add version.go
    Signed-off-by: Dominik Schulz <dominik.schulz@gauner.org>

---
+++ version.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/blang/semver/v4"
+)
+
+func getVersion() semver.Version {
+	sv, err := semver.Parse(strings.TrimPrefix(version, "v"))
+	if err == nil {
+		return sv
+	}
+
+	return semver.Version{
+		Major: 1,
+		Minor: 15,
+		Patch: 3,
+		Pre: []semver.PRVersion{
+			{VersionStr: "git"},
+		},
+		Build: []string{"HEAD"},
+	}
+}
